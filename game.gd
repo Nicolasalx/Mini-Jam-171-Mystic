c4130426b40111b2765949b3aadf4b65db0e4410
NicolasAlx <nicolas1.alexandre@epitech.eu>
2024-11-10 21:34:31 +0100

    mod: level has been modified

--- game.gd
+++ game.gd
@@ -4,11 +4,17 @@ var pressed_buttons = []
 var buttons_level = []
 var level_completed = false
 
+var nb_case_level_1 = 1;
+var nb_case_level_2 = 2;
+
 func _ready():
-	main()
+	await main()
 
 func main():
-	await animation_level(1)
+	await animation_level(nb_case_level_1)
+	while not level_completed:
+		await get_tree().process_frame
+	await animation_level(nb_case_level_2)
 
 func add_case_to_list(buttons_list):
 	var random_number = randi_range(1, 5)
@@ -34,15 +40,13 @@ func create_animation_list(buttons_list, nb_case):
 			var animationName = "Case_" + str(number) + "_Animation"
 			$TextureRect/AnimationPlayer.play(animationName)
 			await get_tree().create_timer(2.0).timeout
-		else:
-			print("No match for: ", button_name)
 	end_level(nb_case)
 
 func animation_level(nb_case):
 	await start_level()
 	for i in range(nb_case):
 		add_case_to_list(buttons_level)
-	create_animation_list(buttons_level, nb_case)
+	await create_animation_list(buttons_level, nb_case)
 
 func enable_buttons(target_size: int):
 	for button in $TextureRect.get_children():
@@ -50,16 +54,17 @@ func enable_buttons(target_size: int):
 			button.connect("pressed", Callable(self, "_on_button_pressed").bind(button, target_size))
 
 func _on_button_pressed(button, target_size):
+	if (level_completed):
+		target_size = nb_case_level_2;
 	pressed_buttons.append(str(button.name))
 	if pressed_buttons.size() == target_size:
 		if pressed_buttons.duplicate() == buttons_level.duplicate():
-			print("SAME")
 			pressed_buttons.clear()
 			buttons_level.clear()
 			$TextureRect/AnimationPlayer.play("level_up")
-			level_completed = true;
-			await get_tree().create_timer(2.0).timeout
-			return
+			level_completed = true
+			if (target_size == nb_case_level_2):
+				get_tree().change_scene_to_file("res://victory_end.tscn")
 		else:
 			get_tree().change_scene_to_file("res://defeat_end.tscn")
 		pressed_buttons.clear()
